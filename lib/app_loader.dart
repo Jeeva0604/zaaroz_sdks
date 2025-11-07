ac7cbe7c9fe585b3794a75fa459fb9ff8e1bc2a6
Jeeva0604 <jeevajeeva060402@gmail.com>
2025-11-07 14:04:05 +0530

    fixed loader

--- lib/app_loader.dart
+++ lib/app_loader.dart
@@ -1,19 +1,46 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:zaaroz_sdk/constant/app_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class AppLoader extends StatelessWidget {
+import 'constant/app_icons.dart';
+
+class AppLoader extends StatefulWidget {
   final double size;
   final Color color;
   const AppLoader({super.key, required this.size, required this.color});
 
   @override
+  State<AppLoader> createState() => _AppLoaderState();
+}
+
+class _AppLoaderState extends State<AppLoader>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat(); // Infinite rotation
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SvgPicture.string(
-      AppIcons.appLoaderSvg,
-      width: size,
-      height: size,
-      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+    return RotationTransition(
+      turns: _controller,
+      child: SvgPicture.string(
+        AppIcons.appLoaderSvg,
+        width: widget.size,
+        height: widget.size,
+        colorFilter: ColorFilter.mode(widget.color, BlendMode.srcIn),
+      ),
     );
   }
 }
